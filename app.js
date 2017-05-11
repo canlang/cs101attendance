002ac41f61ab75f82164fb266d3bfabb6c582211
SangJae Lee <sjlee@SangJaeui-MacBook-Pro.local>
2017-05-11 13:59:53 +0900

    update to G

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var data = require('./src/test.json')
+var data = require('./src/G.json')
 const students = data.map((student) =>
 		({"s_id":student.s_id,
 			"name":student.name,
