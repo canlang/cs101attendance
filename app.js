14c86ae8be89519fa25b4c1a3e39be0c6099bbd5
SangJae Lee <sjlee@SangJaeui-MacBook-Pro.local>
2017-05-15 16:40:16 +0900

    change to section C

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var data = require('./src/G.json')
+var data = require('./src/section_C.json')
 const students = data.map((student) =>
 		({"s_id":student.s_id,
 			"name":student.name,
